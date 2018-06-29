221c24440e21a406cde82172f7b1f07d3818a760
Bart Roos <bart@studiokaa.co>
2018-06-29 15:49:55 +0200

    Create qualification model

--- database/factories/QualificationFactory.php
+++ database/factories/QualificationFactory.php
@@ -13,11 +13,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Qualification::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+    	'crebo' => $faker->randomNumber(5),
+        'title' => strtoupper($faker->word()),
+        'sub_title' => $faker->text(20),
+        'duration' => $faker->numberBetween(1, 3),
+        'terms_per_year' => $faker->numberBetween(2, 4)
     ];
 });
---
+++ database/migrations/2018_06_29_104451_create_qualifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQualificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qualifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('crebo')->nullable();
+            $table->string('title');
+            $table->string('sub_title')->nullable();
+            $table->integer('duration');
+            $table->integer('terms_per_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qualifications');
+    }
+}
