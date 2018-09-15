c5177f5ed7aa3e1db8e13414bdbf9332ea8878fd
Bart Roos <bart@studiokaa.co>
2018-09-15 12:55:28 +0200

    Starting on courses.show

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -7,4 +7,14 @@
 
 @section('content')
 
+	<h2>{{ $course->title }}</h2>
+	<p class="mt-4 mb-0"><i class="far fa-fw fa-user"></i> Vakeigenaar: {{ $course->owner }}</p>
+	<p class="mt-0 mb-3"><i class="far fa-fw fa-folder"></i> Leerlijn: {{ $course->topic->title }}</p>
+	
+	<h3>Planningen</h3>
+	<ul>
+		@foreach($course->terms as $term)
+			<li>{{ $term->cohort->qualification->title }} {{ $term->cohort->title }}, {{ $term->title }}</li>
+		@endforeach
+	</ul>
 @endsection
