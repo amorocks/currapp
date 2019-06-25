e921e6cc6dd3f009d893cb56de1d0fcd9ea7db46
Bart Roos <bartjroos@gmail.com>
2019-06-25 11:27:59 +0200

    Bugfix

--- app/Console/Commands/MysqlDump.php
+++ app/Console/Commands/MysqlDump.php
@@ -39,29 +39,21 @@ class MySqlDump extends Command
      */
     public function handle()
     {
-        $start = date('Y-m-d');
-        $end = date("Y-m-d", strtotime("+2 days"));
+        $ds = DIRECTORY_SEPARATOR;
 
-        $dates = OpeningDates::whereDate('start', '<=', $start)->whereDate('end', '>=', $end)->get();
-        if($dates->count())
-        {
-
-            $ds = DIRECTORY_SEPARATOR;
-
-            $db = config('database.connections.mysql.database');
-            $user = config('database.connections.mysql.username');
-            $pass = config('database.connections.mysql.password');
+        $db = config('database.connections.mysql.database');
+        $user = config('database.connections.mysql.username');
+        $pass = config('database.connections.mysql.password');
 
 
-            $path = database_path() . $ds . 'backups' . $ds . date('Y') . $ds . date('m') . $ds;
-            $file = date('Y-m-d') . '_mysqldump.sql';
-            $command = sprintf('mysqldump %s -u %s -p\'%s\' > %s', $db, $user, $pass, $path . $file);
+        $path = database_path() . $ds . 'backups' . $ds . date('Y') . $ds . date('m') . $ds;
+        $file = date('Y-m-d') . '_mysqldump.sql';
+        $command = sprintf('mysqldump %s -u %s -p\'%s\' > %s', $db, $user, $pass, $path . $file);
 
-            if (!is_dir($path)) {
-                mkdir($path, 0755, true);
-            }
-
-            exec($command);
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
         }
+
+        exec($command);
     }
 }
